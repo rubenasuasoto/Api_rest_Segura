0883370a65b2ec4c70a132d3e60427b6cee3f3b5
ruben asua <rubenasuasoto@gmail.com>
2024-12-16 19:10:17 +0100

    commit

--- src/main/kotlin/com/example/Api_rest_Segura/ApiRestSeguraApplication.kt
+++ src/main/kotlin/com/example/Api_rest_Segura/ApiRestSeguraApplication.kt
@@ -1,9 +1,12 @@
 package com.example.Api_rest_Segura
 
+import com.example.Api_rest_Segura.security.RSAKeysProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(RSAKeysProperties::class)
 class ApiRestSeguraApplication
 
 fun main(args: Array<String>) {
--- src/main/resources/sql/datos-prueba.sql
+++ src/main/resources/sql/datos-prueba.sql
@@ -1,7 +1,7 @@
 ﻿INSERT INTO `usuarios`( `password`, `roles`, `username` )
-VALUES ('1234', 'MECANICO', 'diego');
+VALUES ('$2a$10$DBc2FPq.4XperQMRTGpYnufwdTFxFCJtRZj1zsX.7vFo9YVe9rCyW', 'MECANICO', 'diego');
 
-INSERT INTO `talleres`( `nombreTaller`, `direccion`, `telefono`)
+INSERT INTO `talleres`( `nombre_Taller`, `direccion`, `telefono`)
 VALUES ('FixIt', 'calle margarita', '123456789');
 
 INSERT INTO citas_Taller (`id_usuario`, `id_taller`, `fecha`, `servicio`, `estado`)
